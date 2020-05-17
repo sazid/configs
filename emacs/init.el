7377598aa934105e7ffa8e3418273017b83cadbc
Mohammed Sazid-Al-Rashid <sazidozon@gmail.com>
2020-05-17 22:44:36 +0600

    Add two new themes spacemacs

--- emacs/init.el
+++ emacs/init.el
@@ -18,6 +18,8 @@
 ;; rainbow-delimiters
 ;; tagedit
 ;; magit
+;; spacemacs-theme
+;; color-theme-sanityinc-tomorrow
 
 
 (require 'package)
@@ -38,7 +40,7 @@ There are two things you can do about this warning:
 (package-initialize)
 
 
-(set-frame-font "Roboto Mono 12" nil t)
+(set-frame-font "Roboto Mono 11" nil t)
 
 ;; Turn off the menu bar at the top of each frame because it's distracting
 (menu-bar-mode -1)
@@ -138,9 +140,42 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(beacon-color "#ff9da4")
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+ '(fci-rule-color "#003f8e")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (magit tagedit rainbow-delimiters smex clojure-mode-extra-font-locking expand-region ace-window f helm-projectile popup-complete helm async projectile dash))))
+    (spacemacs-theme color-theme-sanityinc-tomorrow magit tagedit rainbow-delimiters smex clojure-mode-extra-font-locking expand-region ace-window f helm-projectile popup-complete helm async projectile dash)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ff9da4")
+     (40 . "#ffc58f")
+     (60 . "#ffeead")
+     (80 . "#d1f1a9")
+     (100 . "#99ffff")
+     (120 . "#bbdaff")
+     (140 . "#ebbbff")
+     (160 . "#ff9da4")
+     (180 . "#ffc58f")
+     (200 . "#ffeead")
+     (220 . "#d1f1a9")
+     (240 . "#99ffff")
+     (260 . "#bbdaff")
+     (280 . "#ebbbff")
+     (300 . "#ff9da4")
+     (320 . "#ffc58f")
+     (340 . "#ffeead")
+     (360 . "#d1f1a9"))))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -172,3 +207,4 @@ There are two things you can do about this warning:
 
 ;; ace-window
 (global-set-key (kbd "M-o") 'ace-window)
+
